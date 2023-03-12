8d74cb7d65aa6b250a214644d3a298a0f4549968
TohilGuerra <tohil6@gmail.com>
2023-03-12 01:26:32 -0600

    adds objects

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
-// Write your solution in this file!
+const employee = {
+    name: "Tohil",
+    streetAddress: "Rex Ln"
+};
+
+ /*function () {
+    for (const key in employee) {
+      delete employee[key];
+    }
+
+    employee.name = 'Sam';
+  };*/
+
+
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee; 
+  };
+  const newEmployee = updateEmployeeWithKeyAndValue (
+    employee,
+    "name",
+    "Jupiter"
+  );
+  employee.name;
+  newEmployee.name;
+  
+
+  function destructivelyUpdateEmployeeWithKeyAndValue (employee, key, value) {
+    employee[key] = value;
+    return employee;
+  };
+  destructivelyUpdateEmployeeWithKeyAndValue (
+    employee, 
+    "streetAddress",
+    "First Ln"
+    );
+employee.streetAddress;
+
+
+/* function deleteFromEmployeeByKey(employee, key) {
+    const newEmployeE = {...employee} 
+    delete newEmployeE.name;
+    return newEmployeE
+}
+deleteFromEmployeeByKey(employee, "name");
+employee; */
+
+const newEmployeeUpdate = {...employee};
+function deleteFromEmployeeByKey() {
+    delete newEmployeeUpdate.name;
+    return newEmployeeUpdate;
+}
+deleteFromEmployeeByKey ();
+newEmployeeUpdate;
+
+
+function destructivelyDeleteFromEmployeeByKey(employee) {
+    delete employee.name;
+    return employee;
+};
+destructivelyDeleteFromEmployeeByKey(employee, "name");
+employee;
